4e04ec39b58b9ba147932ec5c1fe929c05c1fd35
George Shpak <georgeshpak@Georges-MacBook-Air.local>
2019-06-13 17:55:30 -0700

    Added the questions

--- assets/game.js
+++ assets/game.js
@@ -16,30 +16,32 @@ var questions =[{
 },
 {
     question: "Who holds the record most points scored in one game in the decade?",
-    answers: ["LeBron", "Kobe", "Shaq", "KG"],
+    answers: ["LeBron ", "Kobe ", "Shaq ", "KG "],
     correctAnswer: "Kobe",
 },
 {
     question: "Who did Michael Jordan select with the 1st overall pick?",
-    answers: ["K.Martin", "K.Brown", "D.Howard", "Y.Ming"],
+    answers: ["K.Martin ", "K.Brown ", "D.Howard ", "Y.Ming "],
     correctAnswer: "K.Brown",
 },
 {
     question: "Which NBA player ran into the stands and punch a fan in the face?",
-    answers: ["Kobe", "Carmelo", "Artest", "LeBron"],
+    answers: ["Kobe ", "Carmelo ", "Artest ", "LeBron "],
     correctAnswer: "Artest",
 }];
 
 //Creating function that removes the start button
 $("#start").on('click',function(){
+    $("#start").remove();
     //Looping through the questions
     for(var i =0; i < questions.length; i++){
         $("#play").append('<h3>'+questions[i].question+'</h3>');
-        //looping through the answers
-    for(var j =0; j < questions.answers; j++){
-        $("#play").append("<input type = 'radio' name = 'question-"+i+"'value='" +questions[i].answers[j]+"'>"+questions[i].answers[j]+"'>");
+        //looping through the answers to display and storing the answer in the radio button
+    for(var j =0; j < questions[i].answers.length; j++){
+        $("#play").append("<input type = 'radio' name = 'question-"+i+"'value=' " +questions[i].answers[j]+" ' >"+questions[i].answers[j]+" ");
 
     }
     }
 })
+
 
